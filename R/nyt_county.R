2f4b327a343f48ea42873094a1040f58a8f262d1
Rob <rharbert@stonehill.edu>
2020-03-27 21:26:13 -0400

    fig 2wk

--- R/nyt_county.R
+++ R/nyt_county.R
@@ -110,10 +110,43 @@ a2 = ggplot(cv_ex %>% filter(date == '2020-03-11') %>% filter(!is.na(pop))) +
   xlab('Average Temperature (C)') +
   ylab('Density')
 
+b1 = ggplot(cv_ex %>% filter(date == '2020-03-25') %>% filter(!is.na(pop))) +
+  geom_density(aes(x=wc2.1_10m_tmin_03, weight=(cases/pop)/sum(cases/pop)), colour='darkred', fill='darkred', alpha=0.1)+
+  geom_density(data=all_ex, aes(x=wc2.1_10m_tmin_03, weight=pop/sum(pop)), colour='darkblue', fill='darkblue', alpha=0.1) +
+  theme_minimal() + 
+  xlab('Min Temparature (C)') +
+  ylab('Density')
 
 
-cp = plot_grid(a1,a2, nrow=1, label="AUTO")
 
+b2 = ggplot(cv_ex %>% filter(date == '2020-03-11') %>% filter(!is.na(pop))) +
+  geom_density(aes(x=wc2.1_10m_tmin_03, weight=(cases/pop)/sum(cases/pop)), colour='darkred', fill='darkred', alpha=0.1)+
+  geom_density(data=all_ex, aes(x=wc2.1_10m_tmin_03, weight=pop/sum(pop)), colour='darkblue', fill='darkblue', alpha=0.1) +
+  theme_minimal() + 
+  xlab('Min Temperature (C)') +
+  ylab('Density')
+
+
+c1 = ggplot(cv_ex %>% filter(date == '2020-03-25') %>% filter(!is.na(pop))) +
+  geom_density(aes(x=wc2.1_10m_srad_03, weight=(cases/pop)/sum(cases/pop)), colour='darkred', fill='darkred', alpha=0.1)+
+  geom_density(data=all_ex, aes(x=wc2.1_10m_srad_03, weight=pop/sum(pop)), colour='darkblue', fill='darkblue', alpha=0.1) +
+  theme_minimal() + 
+  xlab('Solar Radiation') +
+  ylab('Density')
+
+
+
+c2 = ggplot(cv_ex %>% filter(date == '2020-03-11') %>% filter(!is.na(pop))) +
+  geom_density(aes(x=wc2.1_10m_srad_03, weight=(cases/pop)/sum(cases/pop)), colour='darkred', fill='darkred', alpha=0.1)+
+  geom_density(data=all_ex, aes(x=wc2.1_10m_srad_03, weight=pop/sum(pop)), colour='darkblue', fill='darkblue', alpha=0.1) +
+  theme_minimal() + 
+  xlab('Solar Radiation') +
+  ylab('Density')
+
+
+cp = plot_grid(a1,a2,b1, b2, c1, c2, ncol=2, nrow=3, label="AUTO")
+
+ggsave(cp, file='compare_2wk.png', height=12, width=9, dpi=600)
 
 ggplot(cv_ex %>% filter(date == '2020-03-12') %>% filter(!is.na(pop))) +
   geom_density(aes(x=wc2.1_10m_tmin_03, weight=(cases/pop)), colour='red') + 
